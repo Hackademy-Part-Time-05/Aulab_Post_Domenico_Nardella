68adb34409e41157d432b7811b80d382b06b0cf4
Domenico50 <125866973+Domenico50@users.noreply.github.com>
2023-05-15 20:42:36 +0200

    form login

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,17 @@
+<x-layout>
+<div class="container-fluid p-5 bg-info text-center text-white">
+    <div class="row justify-content-center">
+        <h1 class="display-1">
+            Login
+        </h1>
+    </div>
+</div>
+
+<div class="container my-5">
+    <div class="row justify-content-center">
+        <div class="col-12 col-md-8">
+            
+        </div>
+    </div>
+</div>
+</x-layout>
