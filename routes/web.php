f6a6301ebeb857b61118f3842cf78c302075c107
Domenico50 <125866973+Domenico50@users.noreply.github.com>
2023-05-23 22:00:43 +0200

    fine user story 3 gestione del writer

--- routes/web.php
+++ routes/web.php
@@ -23,8 +23,11 @@ Route::get('/', function () {
 
 Route::get('/', [App\Http\Controllers\PublicController::class, 'homepage'])->name('homepage');
 
-Route::get('/article/create', [App\Http\Controllers\ArticleController::class, 'create'])->name('article.create');
-Route::post('/article/store', [App\Http\Controllers\ArticleController::class, 'store'])->name('article.store');
+Route::middleware('writer')->group(function(){
+    Route::get('/article/create', [App\Http\Controllers\ArticleController::class, 'create'])->name('article.create');
+    Route::post('/article/store', [App\Http\Controllers\ArticleController::class, 'store'])->name('article.store');
+});
+
 Route::get('/article/index', [App\Http\Controllers\ArticleController::class, 'index'])->name('article.index');
 Route::get('/article/show/{article}', [App\Http\Controllers\ArticleController::class, 'show'])->name('article.show');
 Route::get('/article/category/{category}', [App\Http\Controllers\ArticleController::class, 'byCategory'])->name('article.byCategory');
