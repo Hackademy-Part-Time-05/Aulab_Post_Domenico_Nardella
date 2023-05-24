adf920f5ea2b87bc5f8628d0804be65b8e48666f
Domenico50 <125866973+Domenico50@users.noreply.github.com>
2023-05-24 11:09:17 +0200

    Inizio user story 5 creazione modello tag e relazioni

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -47,11 +47,6 @@
           
           @endguest
         </ul>
-        {{-- <form class="d-flex" role="search">
-          @csrf
-          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success" type="submit">Search</button>
-        </form> --}}
         <form class="d-flex" method="GET" action="{{ route('article.search') }}">
           @csrf
           <input class="form-control me-2" type="search" name="query" placeholder="Cosa stai cercando?" aria-label="Search">
